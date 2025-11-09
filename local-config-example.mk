4188c877870ae176ef831774586957577d0b1c63
Foxie Flakey <foxieflakey@gmail.com>
2025-11-09 14:28:24 +0700

    Add changes for github

--- local-config-example.mk
+++ local-config-example.mk
@@ -3,3 +3,6 @@ site_root := /
 
 # Add additional thing to preprocessor
 preprocess_flags :=
+
+# Set absolute URL referring to site's root including hostname and port
+site_host_root := http://localhost:8080/
