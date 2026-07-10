cb0d4050e37ffb6294ba7baa545e864668dc20b0
Foxie Flakey <foxieflakey@gmail.com>
2026-07-10 15:19:38 +0700

    Delete unused

--- html_preprocess/src/html/util.rs
+++ html_preprocess/src/html/util.rs
@@ -12,14 +12,6 @@ pub fn inc_char_offset(offset: u64, str: &str) -> u64 {
     offset + increment_size
 }
 
-pub fn dec_char_offset(offset: u64, str: &str) -> u64 {
-    (&str[..offset.try_into().unwrap()])
-        .char_indices()
-        .map(|(x, _)| u64::try_from(x).unwrap())
-        .next_back()
-        .unwrap_or(0)
-}
-
 pub fn one_char_span(file: &File, offset: u64) -> Span {
     file.span
         .subspan(offset, inc_char_offset(offset, file.source()))
