3b87e2031d351607844aa0b166afe96f414e226a
Foxie Flakey <foxieflakey@gmail.com>
2026-07-10 15:18:55 +0700

    Remove legacy version

--- html_stuff/src/preprocess/phase1.rs
+++
@@ -1,9 +0,0 @@
-use crate::{html::RootElement, preprocess::EnvValue};
-
-pub fn run<'a, F>(tree: &mut Vec<RootElement<'a>>, mut env_fetcher: F) -> Result<(), String>
-where
-    F: FnMut(&str) -> Option<EnvValue<'a>>,
-{
-    
-    Ok(())
-}
