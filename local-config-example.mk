52b6d710fdb107d643a1fb088b991f73de8baada
Foxie Flakey <foxieflakey@gmail.com>
2026-07-13 17:38:04 +0700

    Nuked everything, we're restarting over

--- local-config-example.mk
+++
@@ -1,8 +0,0 @@
-# Configure which "root" the website going to live under
-site_root := /
-
-# Add additional thing to preprocessor
-preprocess_flags :=
-
-# Set absolute URL referring to site's root including hostname and port
-site_host_root := http://localhost:8080/
